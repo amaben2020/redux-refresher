ab5201a045233145774a68480a79ea13526f2475
algomachine007 <amaben202phantom@gmail.com>
2024-01-06 13:18:23 +0100

    adding success color to selected answer

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { RootState } from "@/redux/store";
+
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
@@ -14,6 +16,7 @@ const Question = () => {
     // console.log("State", state);
     return state.quiz;
   });
+
   console.log(quiz.questions);
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -28,14 +31,12 @@ const Question = () => {
   //   }
   // }, [finalQuestion]);
 
-  if (!isClient) {
-    return null;
-  }
+  const [selectedAnswer, setSelectedAnswer] = useState("");
 
   // ensure that only 1 question is shown per time ✅
-  // make correct answer randomly appear with incorrect ones using random
-  // use progress bar
+  // make correct answer randomly appear with incorrect ones using random ✅
   // when answer is correct, update score by 1
+  // use progress bar
   // click next after answer selection to move to next question, you cannot move back.
 
   const limitQuestion = (array: any, index: number) => {
@@ -46,13 +47,39 @@ const Question = () => {
     return limitedQuestion;
   };
 
+  // correct_answer: 'Red Lion',
+  // incorrect_answers: [ 'Royal Oak', 'White Hart', 'King&#039;s Head' ]
+
+  const shuffle = (array: string[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const renderQuestionsRandomly = (question) => {
+    const { correct_answer, incorrect_answers } = question;
+
+    const questions = [...incorrect_answers, correct_answer];
+
+    return shuffle(questions);
+  };
+
+  const displayColorForQuestion = (question): boolean =>
+    selectedAnswer === question.correct_answer;
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <div className="p-20 border-2 justify-center flex flex-col items-center">
       <h3>Question</h3>
       <p>{quiz?.difficulty}</p>
       <p>{quiz?.questions[0]?.type}</p>
 
-      <p>Score 0/10</p>
+      <p>Score 0 / 10</p>
 
       <div>
         {limitQuestion(quiz?.questions, currentQuestionIndex).map(
@@ -62,10 +89,24 @@ const Question = () => {
                 {currentQuestionIndex + 1}. {question.question}
               </h4>
               <div className="flex flex-col gap-4 my-4">
-                {question.incorrect_answers.map((answer, _) => (
+                {renderQuestionsRandomly(question)?.map((answer, _) => (
                   <>
-                    <button className="border-2 p-3 rounded-lg">
+                    <button
+                      onClick={() => {
+                        setSelectedAnswer(answer);
+                        displayColorForQuestion(question);
+                      }}
+                      className={`${
+                        selectedAnswer === answer
+                          ? "bg-green-600"
+                          : "bg-red-600"
+                      } ${
+                        !selectedAnswer.length && "!bg-white"
+                      } border-2 p-3 rounded-lg`}
+                    >
                       {answer}
+
+                      {selectedAnswer === answer ? "✅" : "❌"}
                     </button>
                   </>
                 ))}
