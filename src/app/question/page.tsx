16310c643e56a5f6560db81d4a5a929c2e8abc6c
algomachine007 <amaben202phantom@gmail.com>
2024-01-06 11:20:04 +0100

    adding questions

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,18 +1,49 @@
 "use client";
 import { RootState } from "@/redux/store";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Question = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const quiz = useSelector((state: RootState) => {
     // console.log("State", state);
     return state.quiz;
   });
-  console.log(quiz.difficulty);
   console.log(quiz.questions);
+
+  if (!isClient) {
+    return null;
+  }
+
+  // make correct answer randomly appear with incorrect ones using random
+  // ensure that only 1 question is shown per time
+  // use progress bar
+  // when answer is correct, update score by 1
+  // click next after answer selection to move to next question, you cannot move back.
 
   return (
     <div className="p-20">
-      Question {quiz?.difficulty} -- {JSON.stringify(quiz.questions)}
+      {/* Question {quiz?.difficulty} -- {JSON.stringify(quiz.questions)} */}
+
+      <h3>Question</h3>
+      <p>{quiz?.difficulty}</p>
+      <p>{quiz?.questions[0]?.type}</p>
+
+      <div>
+        {quiz?.questions.map((question) => (
+          <>
+            <h4>{question.question}</h4>
+            {question.incorrect_answers.map((answer) => (
+              <button>{answer}</button>
+            ))}
+          </>
+        ))}
+      </div>
     </div>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import quizReducer from "./features/quiz/quizSlice";
-
-// export const store = configureStore({
-//   reducer: {
-//     quiz: quizReducer,
-//   },
-// });
-
-// // Infer the type of makeStore
-// export type AppStore = ReturnType<typeof store.getState>;
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {
   FLUSH,
@@ -41,7 +26,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-
+  devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
