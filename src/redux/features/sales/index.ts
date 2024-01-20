e9c50b27ec3012ac2747bf2557ea5500a2941c5c
algomachine007 <amaben202phantom@gmail.com>
2024-01-20 16:58:37 +0100

    concluding addCase for sales

--- src/redux/features/sales/index.ts
+++ src/redux/features/sales/index.ts
@@ -1,25 +1,43 @@
+import { getSales } from "@/services/api/sales";
 import { createSlice } from "@reduxjs/toolkit";
 
 type TInitialState = {
   id: string;
   name: string;
   data: Array<number>;
+  loading: boolean;
+  error: string | undefined;
 };
 
 const initialState: TInitialState = {
   id: "",
   name: "",
   data: [],
+  loading: false,
+  error: "",
 };
 
 const salesSlice = createSlice({
   name: "sales",
   initialState,
   reducers: {},
-  extraReducers: () => {
-    
+  extraReducers: (builder) => {
+    builder.addCase(getSales.pending, (state) => {
+      state.loading = true;
+    });
+
+    builder.addCase(getSales.fulfilled, (state, action) => {
+      console.log(action);
+      state.data = action.payload;
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.loading = false;
+    });
+
+    builder.addCase(getSales.rejected, (state, action) => {
+      state.error = action.error as string;
+    });
   },
 });
 
-export default salesSlice.reducer
-
+export default salesSlice.reducer;
